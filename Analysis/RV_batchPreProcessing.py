0583101ffd406fdabdb357ccb77e24c04834e07d
lory <11058498+lory3587@users.noreply.github.com>
2016-04-01 11:37:23 +0100

    Updated preprocessing scripts in order to output files to Analysis folder.

--- Analysis/RV_batchPreProcessing.py
+++ Analysis/RV_batchPreProcessing.py
@@ -9,12 +9,12 @@ import subprocess
 import sys
 
 # Specify the folder list file
-folderListFile = 'C:/repositories/Rat Videogame/Analysis/Folder Lists/GenericFolderList.txt'
+folderListFile = r'D:/Repositories/Rat_Videogame/Analysis/Folder Lists/GenericFolderList.txt'
 
 # Specify relevant  Paths
-bonsaiPath = r'C:\Users\Adam\AppData\Local\Bonsai\Bonsai64.exe'
+bonsaiPath = r'C:\Users\IntelligentSystems\AppData\Local\Bonsai\Bonsai64.exe'
 #bonsaiPath = r'C:\Users\Lory\AppData\Local\Bonsai\Bonsai64.exe'???
-workflowPath = r'C:/repositories/Rat Videogame/Analysis/RV_preProcessing.bonsai'
+workflowPath = r'D:/Repositories/Rat_Videogame/Analysis/RV_preProcessing.bonsai'
 
 # Read the folder list file
 folderFile = open(folderListFile, "r")
@@ -22,22 +22,44 @@ folderList = folderFile.readlines()
 folderFile.close()
 
 # Go through every "session" folder and pre-process the IR video
-for sessionFolder in folderList:
-
-    # Remove whitespace from string
-    sessionFolder = sessionFolder.strip()
-
-    # Report progress
-    print "Analyzing " + sessionFolder + "!"
-    sys.stdout.flush()
-    
-    # Create the Bonsai command line params
-    moviePath  = sessionFolder+ r'\IR.avi'
-    argFilePath = r'-p:FileName='  + moviePath
-
-    # Run Bonsai (in Session Folder) with command line Args
-    os.chdir(sessionFolder)
-    subprocess.call([bonsaiPath, workflowPath, argFilePath, r'--noeditor'])
+for subjectFolder in folderList:
+    sessionFolders = os.listdir(subjectFolder.rstrip('\n'))
+    for sessionFolder in sessionFolders:            
+        try:
+            # Remove whitespace from string
+            sessionFolder = sessionFolder.strip()
+            sessionFolder = os.path.join(subjectFolder.rstrip('\n'),
+                                         sessionFolder)
+                                         
+            # Generate analysis folder
+            analysisFolder = os.path.join(sessionFolder, 'Analysis')
+            if not os.path.exists(analysisFolder):
+                os.mkdir(analysisFolder)
+            else:
+                print "Skipped... folder already exists!"
+                continue
+        
+            # Report progress
+            print "Analyzing " + sessionFolder + "!"
+            sys.stdout.flush()
+            
+            # Create the Bonsai command line params
+            moviePath  = r'IR.avi'
+            argFilePath = r'-p:FileName='  + moviePath
+        
+            # Run Bonsai (in Session Folder) with command line Args
+            os.chdir(sessionFolder)
+            print moviePath
+            subprocess.call([bonsaiPath, workflowPath, argFilePath, r'--noeditor'])
+        except Exception as e:
+            print e.__doc__
+            print e.message
+            continue
     
 print "Done!!"
 # FIN
+
+for subjectFolder in folderList:
+    sessionFolders = os.listdir(subjectFolder.rstrip('\n'))
+    for sessionFolder in sessionFolders:
+        print sessionFolder
